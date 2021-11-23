e913783538df80208ede9643af097113f5b6630f
KamiliaM <kamilia.maizia@edu.univ-paris13.fr>
2021-11-23 14:06:48 +0100

    tp
    tp

--- tp3/js/form-validation.js
+++ tp3/js/form-validation.js
@@ -1,5 +1,16 @@
-
+window.onload = function () {   // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" )//inspecter console on aura l'affichage de DOM ready
+     
+     // Y mettre le code Javascript pour valider tous les champs du formulaire
+ };
     
+    
+    function validateEmail(mail) {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(mail).toLowerCase());
+        }
+         
     function validation()
     {
         var name = document.getElementById("name").value;   
@@ -23,7 +34,7 @@
             //document.getElementById("error").innerHTML="La saisie du nom est obligatoire"; 
             //document.getElementById("error").style.display = 'block';                                
             // myForm["name"].focus(); // Focus
-           alert("Mettez votre nom."); 
+         
            
             return false; 
             
@@ -42,7 +53,7 @@
             // document.getElementById("error").innerHTML="La saisie du prenom est obligatoire";    
             // document.getElementById("error").style.display = 'block' ;                          
             //myForm["prenom"].focus(); // Focus
-            alert("Mettez votre prenom.");            
+                   
             
            
             return false; 
@@ -60,7 +71,6 @@
             //document.getElementById("error").innerHTML="La saisie de la date de naissance est obligatoire";  
            // document.getElementById("error").style.display = 'block' ;                              
            // myForm["datenaissance"].focus(); // Focus
-            alert("Mettez votre date de naissance."); 
                      
             return false;
 
@@ -83,7 +93,6 @@
             //document.getElementById("error").innerHTML="La saisie de l'adresse est obligatoire";   
             //document.getElementById("error").style.display = 'block';                           
             //myForm["adresse"].focus(); // Focus
-            alert("Mettez votre adresse.");
         
            
             return false; 
@@ -101,47 +110,44 @@
         if ( mail== "")                                  
         { 
             myModal.show();
-            //document.getElementById("error").innerHTML="La saisie du mail est obligatoire";  
-            //document.getElementById("error").style.display = 'block' ;                             
-            //myForm["dmail"].focus(); // Focus
-            alert("Mettez votre adresse mail."); 
+            document.getElementById("error").innerHTML="La saisie du mail est obligatoire";  
+            document.getElementById("error").style.display = 'block' ;                             
+            myForm["dmail"].focus(); // Focus
             
            
             return false; 
         } else
             if(mail.length<5)
         {
-            //document.getElementById("error").innerHTML="les champs mail doivent avoir 5 caractères mininum";
-            //document.getElementById("error").style.display = 'block';                                 
+            document.getElementById("error").innerHTML="les champs mail doivent avoir 5 caractères mininum";
+            document.getElementById("error").style.display = 'block';                                 
     
             return false; 
         }else if(!validateEmail(mail))
         {
 
-            //document.getElementById("error").innerHTML="Synthaxe incorrecte";
-            //document.getElementById("error").style.display = 'block';                                 
+            document.getElementById("error").innerHTML="Synthaxe du mail est incorrecte";
+            document.getElementById("error").style.display = 'block';                                 
     
             return false; 
         }
        
      
-      document.getElementById("resultat").innerHTML="Bienvenue Vos Informations sont Validées"+" "+name+" "+prenom ; 
+      document.getElementById("resultat").innerHTML="Bienvenue"+" "+name+" "+prenom+" Vos Informations sont Validées" ; 
       document.getElementById("resultat").style.display = 'block' ;    
       
       document.querySelector(".modal-body .date").textContent = dateNaissance.toLocaleDateString();
       document.querySelector(".modal-body img").src = ' https://maps.googleapis.com/maps/api/staticmap?markers='+adresse+'&zoom=7&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg ';
-        
+  
+   
+  document.querySelector(".modal-title .prenom").textContent ="Bienvenighjkue"+prenom;
+      //document.querySelector(".modal-title .prenom").textContent ="Bienvenue"+"  "+prenom+" Vos Informations sont Validées" ; 
 
-      
       myModal1.show();
      return true;
+    
 
-
-     function validateEmail(mail) {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(mail).toLowerCase());
-    }
-     
+    
     
   
 };
